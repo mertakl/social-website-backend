a93643c18193bc250e91ac2c377d3457156f1b2a
Mert Akel <akelmert95@gmail.com>
2019-09-17 17:16:38 +0300

    Update config.js

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,10 +1,1 @@
-module.exports = {
-    apiKey: "AIzaSyDVgClB7CTNmNFoVOU2dJqu_Ke3vEOV_6c",
-    authDomain: "socialape-6588b.firebaseapp.com",
-    databaseURL: "https://socialape-6588b.firebaseio.com",
-    projectId: "socialape-6588b",
-    storageBucket: "socialape-6588b.appspot.com",
-    messagingSenderId: "56569186201",
-    appId: "1:56569186201:web:e6061c63e306ec7e0eb508"
-};
 
